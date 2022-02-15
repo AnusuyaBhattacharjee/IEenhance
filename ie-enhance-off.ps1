5a2790c061cabe2fda3a715530f697b6700a2072
AnusuyaBhattacharjee <anubhat@microsoft.com>
2022-02-15 17:27:49 +0530

    Update ie-enhance-off.ps1

--- ie-enhance-off.ps1
+++ ie-enhance-off.ps1
@@ -7,4 +7,4 @@ Stop-Process -Name Explorer
 Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
 }
 Disable-IEESC
-Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+#####Install-WindowsFeature -Name Web-Server -IncludeManagementTools######
